a576d5d2f816ac6c982bafa59445e03c744eb786
yoosple <103109240+yoosple@users.noreply.github.com>
2022-05-01 20:06:16 +0900

    Time: 157 ms (70.43%), Space: 43.2 MB (6.09%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cs
+++ 1122-relative-sort-array/1122-relative-sort-array.cs
@@ -1,35 +1,33 @@
 public class Solution {
     public int[] RelativeSortArray(int[] arr1, int[] arr2) {
-            Queue<int> temp = new Queue<int>();
-            List<int> listA = new List<int>(arr1);
-            List<int> listB = new List<int>(arr2);
-            List<int> orverlap = listA.Where(x => listB.Select(y => y).Contains(x)).ToList();
-            List<int> noneOrverlap = listA.Where(x => !listB.Select(y => y).Contains(x)).ToList();
-            noneOrverlap = noneOrverlap.OrderBy(x => x).ToList();
+        Queue<int> temp = new Queue<int>();
+        List<int> orverlap = arr1.Where(x => arr2.Select(y => y).Contains(x)).ToList();
+        List<int> noneOrverlap = arr1.Where(x => !arr2.Select(y => y).Contains(x)).ToList();
+        noneOrverlap = noneOrverlap.OrderBy(x => x).ToList();
 
-            for(int i = 0; i < arr2.Length; i++)
+        for(int i = 0; i < arr2.Length; i++)
+        {
+            for(int j = 0; j < orverlap.Count; j++)
             {
-                for(int j = 0; j < orverlap.Count; j++)
+                if (arr2[i] == orverlap[j])
                 {
-                    if (arr2[i] == orverlap[j])
-                    {
-                        temp.Enqueue(orverlap[j]);
-                    }
+                    temp.Enqueue(orverlap[j]);
                 }
             }
+        }
 
-            for(int i = 0; i < noneOrverlap.Count; i++)
-            {
-                temp.Enqueue(noneOrverlap[i]);
-            }
+        for(int i = 0; i < noneOrverlap.Count; i++)
+        {
+            temp.Enqueue(noneOrverlap[i]);
+        }
 
-            int[] result = new int[temp.Count()];
-            int q = 0;
-            while(temp.Count() > 0)
-            {
-                result[q] = temp.Dequeue();
-                q++;
-            }
+        int[] result = new int[temp.Count()];
+        int q = 0;
+        while(temp.Count() > 0)
+        {
+            result[q] = temp.Dequeue();
+            q++;
+        }
             
         return result;
     }
