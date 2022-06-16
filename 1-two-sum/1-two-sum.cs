b303380999276ab1d7950b515a2a09057f5cebf4
yoosple <103109240+yoosple@users.noreply.github.com>
2022-06-16 18:25:30 +0900

    Time: 276 ms (21.83%), Space: 43.2 MB (50.32%) - LeetHub

--- 1-two-sum/1-two-sum.cs
+++ 1-two-sum/1-two-sum.cs
@@ -1,15 +1,19 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {        
-        Dictionary<int,int> dic = new Dictionary<int,int>();
-        
-        for(int i = 0; i < nums.Length; i++)
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
         {
-            if(dic.ContainsKey(target - nums[i]))
-                return new int[]{i, dic[target - nums[i]]};
-            else if(!dic.ContainsKey(nums[i]))
+            if (dic.ContainsKey(target - nums[i])) 
+            {
+                return new int[] { dic[target - nums[i]], i };
+            }
+            else if (!dic.ContainsKey(nums[i])) 
+            {
                 dic.Add(nums[i], i);
+            }
         }
-        
+
         return new int[2];
     }
 }
