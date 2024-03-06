cb9a4755ce3bc6c8f5de611cd0911e29823e6a43
유지혜 <103109240+yoosple@users.noreply.github.com>
2024-03-06 22:25:44 +0900

    Update 0014-longest-common-prefix.cs

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -1,33 +1,33 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        int total = strs.Length;
+        if (strs == null || strs.Length == 0)
+            return string.Empty;
+
+        
         string result = string.Empty;
         Dictionary<string, int> dictionary = new Dictionary<string, int>();
 
-        if (total > 0)
+        string first = strs[0];
+        for (int i = 0; i < first.Length; i++)
         {
-            string first = strs[0];
-            for (int i = 0; i < first.Length; i++)
-            {
-                dictionary.Add(first.Substring(0, i + 1), 1);
-            }
+            dictionary.Add(first.Substring(0, i + 1), 1);
+        }
 
-            for (int strsCount = 1; strsCount < total; strsCount++)
+        for (int strsCount = 1; strsCount < strs.Length; strsCount++)
+        {
+            string second = strs[strsCount];
+            for (int j = 0; j < second.Length; j++)
             {
-                string second = strs[strsCount];
-                for (int j = 0; j < second.Length; j++)
+                string s = second.Substring(0, j + 1);
+                if (dictionary.ContainsKey(s))
                 {
-                    string s = second.Substring(0, j + 1);
-                    if (dictionary.ContainsKey(s))
-                    {
-                        dictionary[s]++;
-                    }
+                    dictionary[s]++;
                 }
             }
-
-            var prefix = dictionary.LastOrDefault(item => item.Value == total);
-            result = prefix.Key ?? result;
         }
+
+        var prefix = dictionary.LastOrDefault(item => item.Value == strs.Length);
+        result = prefix.Key ?? result;        
 
         return result;
     }
