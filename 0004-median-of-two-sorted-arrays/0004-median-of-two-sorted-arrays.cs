02bb581e73cd43a1131aa0fe32c29f6852c7d3d0
유지혜 <103109240+yoosple@users.noreply.github.com>
2024-08-07 10:04:46 +0900

    Time: 98 ms (45.85%), Space: 56.3 MB (8.75%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merged = nums1.Concat(nums2).ToArray();
+        merged = merged.OrderBy(x => x).ToArray();
+
+        if (merged.Length % 2 == 0)
+        {
+            int a = merged.Length / 2;
+            int b = a - 1;
+            double result = ((double)merged[b] + (double)merged[a]) / 2;
+            return result;
+        }
+        else
+        {
+            int a = (merged.Length - 1) / 2;
+
+            return merged[a];
+        }
+    }
+}
